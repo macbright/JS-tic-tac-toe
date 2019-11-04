fe57e0583afe40a17aaf9000db93d294bf2d1751
bright okike <brightokike@gmail.com>
2019-11-04 15:20:38 +0300

    the gameboard object

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const player2Next = document.querySelector(".player2-next")
 const userInfo = document.querySelector(".user-info")
 const player2 = document.querySelector(".player2")
 const player1 = document.querySelector(".player1")
-const gameBoard = document.querySelector(".game-board")
+const boardLayout = document.querySelector(".game-board")
 player1Next.addEventListener('click', () => {
     player1.style.display = "none"
     player1Next.style.display = "none"
@@ -13,5 +13,48 @@ player1Next.addEventListener('click', () => {
 
 player2Next.addEventListener('click', () => {
     userInfo.style.display = "none"
-    gameBoard.style.display = 'block'
+    boardLayout.style.display = 'block'
 });
+
+
+const gameBoard = (() => {
+    let board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
+    const addPosition = (position, mark) => {
+        board[position - 1] = mark;
+    };
+
+    const resetBoard = () => {
+        board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
+        return board;
+    };
+
+    const render = () => {
+        let one = document.querySelector('.one')
+        let two = document.querySelector('.two')
+        let three = document.querySelector('.three')
+        let four = document.querySelector('.four')
+        let five = document.querySelector('.five')
+        let six = document.querySelector('.six')
+        let seven = document.querySelector('.seven')
+        let eight = document.querySelector('.eight')
+        let nine = document.querySelector('.nine')
+
+        return [
+            one.innerHTML = 'X',
+            two.innerHTML = 'O',
+            three.innerHTML = 'X',
+            four.innerHTML = 'O',
+            five.innerHTML = 'X',
+            six.innerHTML = 'O',
+            seven.innerHTML = 'X',
+            eight.innerHTML = 'O',
+            nine.innerHTML = 'X'
+        ]
+    }
+    return {
+        board,
+        addPosition,
+        resetBoard,
+        render
+    };
+})();
