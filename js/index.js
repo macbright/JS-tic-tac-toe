e9a0f75aa4f193f9a6c79ceb38ec6b20a29bda3f
addo <forison6205@gmail.com>
2019-11-04 06:26:39 -0500

    gameboard ui

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const player2Next = document.querySelector(".player2-next")
 const userInfo = document.querySelector(".user-info")
 const player2 = document.querySelector(".player2")
 const player1 = document.querySelector(".player1")
+const gameBoard = document.querySelector(".game-board")
 player1Next.addEventListener('click', () => {
     player1.style.display = "none"
     player1Next.style.display = "none"
@@ -12,5 +13,5 @@ player1Next.addEventListener('click', () => {
 
 player2Next.addEventListener('click', () => {
     userInfo.style.display = "none"
-    player1.style.display = 'block'
+    gameBoard.style.display = 'block'
 });
